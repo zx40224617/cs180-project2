1f363af76fa6d118d817d1825c355c3185c07f0e
40224617 <zx96748520@gmail.com>
2024-09-24 01:54:46 -0700

    fixed

--- app/accessories/Sidebar.tsx
+++ app/accessories/Sidebar.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import { ListItemButton } from "@mui/material";
 import ListItemText from "@mui/material/ListItemText";
 
